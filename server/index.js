0c014b19f4a417d7b106a65af10d2ec8a6eef991
rayaneBouzroura <2111752@cegepmontpetit.ca>
2024-06-04 17:35:48 -0400

    commit log mongoose connect

--- server/index.js
+++ server/index.js
@@ -20,9 +20,13 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => {
+    console.log('Connected to MongoDB');
+  }).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
 app.get("/getCompte", (req, res) => {
      CompteModel.find({}, (err, result) => {
